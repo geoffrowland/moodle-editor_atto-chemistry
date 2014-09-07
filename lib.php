14d354c7e77b3148804c6d10f61e5bf13198ad24
geoffrowland <rowland.geoff@gmail.com>
2014-09-07 18:30:00 +0100

    Initial commit of chemistry editor

---
+++ lib.php
@@ -0,0 +1,106 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Atto text editor integration version file.
+ *
+ * @package    atto_chemistry
+ * @copyright  2014 Geoffrey Rowland <rowland.geoff@gmail.com>
+ * Based on    @package atto_equation
+ * @copyright  2013 Damyon Wiese <damyon@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Get the list of strings for this plugin.
+ * @param string $elementid
+ */
+function atto_chemistry_strings_for_js() {
+    global $PAGE;
+
+    $PAGE->requires->strings_for_js(array('savechemistry',
+                                          'editchemistry',
+                                          'preview',
+                                          'cursorinfo',
+                                          'update',
+                                          'librarygroup1',
+                                          'librarygroup2',
+                                          'librarygroup3',
+                                          'librarygroup4',
+                                          'librarygroup5',
+                                          'librarygroup6',
+                                          'librarygroup7'),
+                                    'atto_chemistry');
+}
+
+/**
+ * Set params for this plugin.
+ *
+ * @param string $elementid
+ * @param stdClass $options - the options for the editor, including the context.
+ * @param stdClass $fpoptions - unused.
+ */
+function atto_chemistry_params_for_js($elementid, $options, $fpoptions) {
+    $texexample = '$$\pi$$';
+
+    // Format a string with the active filter set.
+    // If it is modified - we assume that some sort of text filter is working in this context.
+    $result = format_text($texexample, true, $options);
+    $texfilteractive = ($texexample !== $result);
+    // $texfilteractive = 1;
+    $context = $options['context'];
+        if (!$context) {
+        $context = context_system::instance();
+    }
+
+    // Tex example librarys.
+    $library = array(
+            'group1' => array(
+                'groupname' => 'librarygroup1',
+                'elements' => get_config('atto_chemistry', 'librarygroup1'),
+            ),
+            'group2' => array(
+                'groupname' => 'librarygroup2',
+                'elements' => get_config('atto_chemistry', 'librarygroup2'),
+            ),
+            'group3' => array(
+                'groupname' => 'librarygroup3',
+                'elements' => get_config('atto_chemistry', 'librarygroup3'),
+            ),
+            'group4' => array(
+                'groupname' => 'librarygroup4',
+                'elements' => get_config('atto_chemistry', 'librarygroup4'),
+            ),
+            'group5' => array(
+                'groupname' => 'librarygroup5',
+                'elements' => get_config('atto_chemistry', 'librarygroup5'),
+            ),
+            'group6' => array(
+                'groupname' => 'librarygroup6',
+                'elements' => get_config('atto_chemistry', 'librarygroup6'),
+            ),
+            'group7' => array(
+                'groupname' => 'librarygroup7',
+                'elements' => get_config('atto_chemistry', 'librarygroup7'),
+            ));
+
+    return array('texfilteractive' => $texfilteractive,
+                 'contextid' => $context->id,
+                 'library' => $library,
+                 'texdocsurl' => get_docs_url('Chemistry_notation_using_mhchem'));
+}
---
+++ settings.php
@@ -0,0 +1,473 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Settings that allow configuration of the list of tex examples in the chemistry editor.
+ *
+ * @package    atto_chemistry
+ * @copyright  2014 Geoffrey Rowland <rowland.geoff@gmail.com>
+ * Based on    @package atto_equation
+ * @copyright  2013 Damyon Wiese <damyon@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$ADMIN->add('editoratto', new admin_category('atto_chemistry', new lang_string('pluginname', 'atto_chemistry')));
+
+$settings = new admin_settingpage('atto_chemistry_settings', new lang_string('settings', 'atto_chemistry'));
+if ($ADMIN->fulltree) {
+    // Group 1
+    $name = new lang_string('librarygroup1', 'atto_chemistry');
+    $desc = new lang_string('librarygroup1_desc', 'atto_chemistry');
+    $default = '
+X^{+}
+X^{2+}
+Y^{3+}
+X^{-}
+X^{2-}
+X^{3-}
+X_{a}
+X_{a}Y
+X_{a}Y_{b}
+^{a}X
+^{a}_{b}X
+^{a}_{b}X^{c}
+\overset{a}{X}
+\underset{b}{X}
+\overset{a}{\underset{b}{X}}
+\pm
++
+2+
+3+
+$\oplus$
+$\ominus$
+-
+2-
+3-
+e^-
+I
+II
+III
+IV
+V
+VI
+VII
+VIII
+IX
+X
+(s)
+(l)
+(g)
+(aq)
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup1',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+    // Group 2
+    $name = new lang_string('librarygroup2', 'atto_chemistry');
+    $desc = new lang_string('librarygroup2_desc', 'atto_chemistry');
+    $default = '
+.
+:
+°
+°C
+°F
+^{⦵}
+$\oplus$
+$\ominus$
+A/B
+\frac{[A][B]}{[C]}
+\frac{[A][B]}{[C][D]}
+$K$_{c}
+$K$_{a}
+$K$_{w}
+$K$_{stab}
+\Delta{$H$}
+\Delta{$H$}_{r}
+\Delta{$H$}_{c}
+\Delta{$H$}_{f}
+\Delta{$H$}^{⦵}_{r}
+\Delta{$H$}^{⦵}_{c}
+\Delta{$H$}^{⦵}_{f}
+\Delta{$G$}
+$T$
+\Delta{$S$}
+$E$^{⦵}
+$A$_{r}
+$M$_{r}
+pH
+p$K$_{a}
+log
+log_{10}
+mol
+mol^{-1}
+dm^3
+dm^{-3}
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup2',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+    // Group 3
+    $name = new lang_string('librarygroup3', 'atto_chemistry');
+    $desc = new lang_string('librarygroup3_desc', 'atto_chemistry');
+    $default = '
+\bond{-}
+\bond{=}
+\bond{#}
+\bond{~}
+\bond{~-}
+\bond{~=}
+\bond{-~-}
+\bond{...}
+\bond{....}
+\bond{->}
+\bond{<-}
+->
+<-
+<->
+<=>
+<=>>
+<<=>
+->[above]
+->[above][below]
+->[][below]
+<=>[above]
+<=>[above][below]
+<=>[][below]
+^
+v
+$\upharpoonleft$
+$\downharpoonright$
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup3',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+    // Group 4
+    $name = new lang_string('librarygroup4', 'atto_chemistry');
+    $desc = new lang_string('librarygroup4_desc', 'atto_chemistry');
+    $default = '
+\alpha
+\beta
+\gamma
+\delta
+\epsilon
+\varepsilon
+\zeta
+\eta
+\theta
+\vartheta
+\iota
+\kappa
+\varkappa
+\lambda
+\mu
+\nu
+\xi
+\omicron
+\pi
+\varpi
+\rho
+\varrho
+\sigma
+\varsigma
+\tau
+\upsilon
+\phi
+\varphi
+\chi
+\psi
+\omega
+A
+B
+\Gamma
+\Delta
+E
+Z
+H
+\Theta
+I
+K
+\Lambda
+M
+N
+\Xi
+O
+\Pi
+P
+\Sigma
+T
+\Upsilon
+\Phi
+X
+\Psi
+\Omega
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup4',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+// Group 5
+    $name = new lang_string('librarygroup5', 'atto_chemistry');
+    $desc = new lang_string('librarygroup5_desc', 'atto_chemistry');
+    $default = '
+$a$
+$b$
+$c$
+$d$
+$e$
+$f$
+$g$
+$h$
+$i$
+$j$
+$k$
+$l$
+$m$
+$n$
+$o$
+$p$
+$q$
+$r$
+$s$
+$t$
+$u$
+$v$
+$w$
+$x$
+$y$
+$z$
+$A$
+$B$
+$C$
+$D$
+$E$
+$F$
+$G$
+$H$
+$I$
+$J$
+$K$
+$L$
+$M$
+$N$
+$O$
+$P$
+$Q$
+$R$
+$S$
+$T$
+$U$
+$V$
+$W$
+$X$
+$Y$
+$Z$
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup5',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+// Group 6
+    $name = new lang_string('librarygroup6', 'atto_chemistry');
+    $desc = new lang_string('librarygroup6_desc', 'atto_chemistry');
+    $default = '
+H
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+He
+Li
+Be
+
+
+
+
+
+
+
+
+
+
+B
+C
+N
+O
+F
+Ne
+Na
+Mg
+
+
+
+
+
+
+
+
+
+
+Al
+Si
+P
+S
+Cl
+Ar
+K
+Ca
+Sc
+Ti
+V
+Cr
+Mn
+Fe
+Co
+Ni
+Cu
+Zn
+Ga
+Ge
+As
+Se
+Br
+Kr
+Rb
+Sr
+Y
+Zr
+Nb
+Mo
+Tc
+Ru
+Rh
+Pd
+Ag
+Cd
+In
+Sn
+Sb
+Te
+I
+Xe
+Cs
+Ba
+$\dagger$
+Hf
+Ta
+W
+Re
+Os
+Ir
+Pt
+Au
+Hg
+Tl
+Pb
+Bi
+Po
+At
+Rn
+Fr
+Ra
+$\ddagger$
+Rf
+Db
+Sg
+Bh
+Hs
+Mt
+Ds
+Rg
+Cn
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup6',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+// Group 7
+    $name = new lang_string('librarygroup7', 'atto_chemistry');
+    $desc = new lang_string('librarygroup7_desc', 'atto_chemistry');
+    $default = '
+$\dagger$
+La
+Ce
+Pr
+Nd
+Pm
+Sm
+Eu
+Gd
+Tb
+Dy
+Ho
+Er
+Tm
+Yb
+Lu
+
+
+$\ddagger$
+Ac
+Th
+Pa
+U
+Np
+Pu
+Am
+Cm
+Bk
+Cf
+Es
+Fm
+Md
+No
+Lr
+';
+    $setting = new admin_setting_configtextarea('atto_chemistry/librarygroup7',
+                                                $name,
+                                                $desc,
+                                                $default);
+    $settings->add($setting);
+
+}
