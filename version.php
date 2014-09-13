c89c166b833b1e5d605ffcfb92e9ad390e5e272a
geoffrowland <rowland.geoff@gmail.com>
2014-09-13 23:25:47 +0100

    Update version.php
    Update to release candidate 2

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014090700;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2014091300;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014050800;        // Requires this Moodle version.
 $plugin->component = 'atto_chemistry';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC; // Release Candidate
-$plugin->release = 'v2.7-rc1'; // This is our first release candidate for the Moodle 2.7.x branch.
+$plugin->release = 'v2.7-rc2'; // This is our second release candidate for the Moodle 2.7.x branch.
