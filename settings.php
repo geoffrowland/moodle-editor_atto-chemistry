9af63f98fad7ec4c9380097e85fe00fcc957772b
Geoff Rowland <rowland.geoff@gmail.com>
2014-10-05 19:07:41 +0100

    Update settings.php
    Add periodic table group numbers. These can also be used in generating formulae.

--- settings.php
+++ settings.php
@@ -300,6 +300,24 @@ ${Z}$
     $name = new lang_string('librarygroup6', 'atto_chemistry');
     $desc = new lang_string('librarygroup6_desc', 'atto_chemistry');
     $default = '
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
 H
 
 
